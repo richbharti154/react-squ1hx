1e2b2d10b33cf9d21e22217d9c0c4936ca8e0aa9
richbharti154 <116936700+richbharti154@users.noreply.github.com>
2023-02-26 15:38:44 +0530

    2 files modified

--- src/Component/Register.js
+++ src/Component/Register.js
@@ -1,122 +1,129 @@
-import React, { useState, useEffect } from 'react';
- import Navbar from './Navbar'
+import React, { useState} from "react";
+import { MdMailOutline } from "react-icons/md";
+import { FaLock ,FaUserAlt} from "react-icons/fa";
+import RegisterStyle from "./Register.module.css"
+import Navbar from './Navbar'
  import {NavLink} from 'react-router-dom'
 import Input from '../Atom/Input';
 import Button from '../Atom/Button';
-import  RegisterStyle from './Register.module.css'
-export default function Register() {
-   JSON.parse(localStorage.getItem('myInput')) || '';
-   const [fname, setFname] = useState("");
-   const [lname, setLname] = useState("");
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
-   const [mark, setMark] = useState('');
-   const [mark2, setMark2] = useState('');
-  useEffect(() => {
-    let newData = {
-      firstname: fname,
-      lastname: lname,
-      myEmail: email,
-      myPassword: password,
-      mark,
-      mark2,
-    };
-    localStorage.getItem('myInput', JSON.stringify(newData));
-  }, [fname, lname, email, password]);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    let newData = {
-      fname: fname,
-      lname: lname,
-      email: email,
-      password: password,
-      mark,
-      mark2,
-    };
-    localStorage.setItem('userList', JSON.stringify(newData));
-    setFname('');
-    setEmail('');
-    setLname('');
-    setPassword('');
-    setMark('');
-    setMark2('');
-  }
-  return (
-    <>
-    <Navbar/>
+
+export default function Register(){
+  const[email,setEmail]=useState("")
+  const[password,setpassword]=useState("")
+  const[username,setusername]=useState("")
+  const[message,setMessage]=useState("")
+  const[pwmessage,setPwMessage]=useState("")
+  const[uMes,setUMes]=useState("")
+  const[data,setData]=useState([])
+  const [show,setShow]=useState(false)
+
    
-      <div className={RegisterStyle.main}>
-       
-     <div className={RegisterStyle.innercontaner1}>
-          <div className={RegisterStyle.header}>Register Form</div>
+  function captureemail(e){
+    setEmail(e.target.value)
+    const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+
+    if (regEx.test(email)) {
+      setMessage("Email is Valid");
+    } else if (!regEx.test(email) && email !== " ") {
+      setMessage("Email is Not Valid");
+    } else {
+      setMessage("");
+    }
+    }
+    function capturepass(e){
+      setpassword(e.target.value)
+      const pwRegEx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,20}$/
+  
+      if (pwRegEx.test(password)) {
+        setPwMessage("pw is Valid");
+        } else if (!pwRegEx.test(password) && password !== " ") {
+          setPwMessage("pw is Not Valid");
+        } else {
+          setPwMessage("");
+        }
+    }
+    function captureuser(e){
+      setusername(e.target.value)
+      const userRegEx= /^[a-zA-Z0-9]+([_\s/\-]?[a-zA-Z0-9])*$/
       
-         <div className={RegisterStyle.innercontaner2}>
-         <form onSubmit={handleSubmit}>
-            <Input
-              onChange={(e) => setFname(e.target.value)}
-              placeholder="Firstname"
-              value={fname}
-              className={RegisterStyle.input1}
-              required
-            />
-            <Input
-              onChange={(e) => setLname(e.target.value)}
-              placeholder="Lastname"
-              value={lname}
-              className={RegisterStyle.input1}
-              required
-            />
-            <Input
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="email"
-              value={email}
-              className={RegisterStyle.input1}
-              required
-            />
-
-            <Input
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="password"
-              value={password}
-              type='password'
-            className={RegisterStyle.input1}
-              required
-            />
-            <span style={{ display: 'flex', gap: '1rem' }}>
-              <Input
-                onChange={(e) => setMark(e.target.value)}
-                value={mark}
-                className={RegisterStyle.input2}
-                type="checkbox"
-                required
-              />
-              <p style={{ fontSize: '9px' }}>
-                open Chrome. At the top right, click More . Click More tools
-                Clear
-              </p>
-            </span>
-
-              <span style={{ display: 'flex', gap: '2px' }}>
-              <Input
-                onChange={(e) => setMark2(e.target.value)}
-                value={mark2}
-                className={RegisterStyle.input2}
-                type="checkbox"
-                required
-              />
-              <p style={{ fontSize: '9px' }}>
-                Next page go to the Emoji search and search your favourite emoji
-                Clear
-              </p>
-            </span>
-            <Button text="Sumbit" className={RegisterStyle.btn} />
-            <NavLink className={RegisterStyle.Emoji} to ='/emoji'>😀Emoji</NavLink>
-            </form>
+  
+      if (userRegEx.test(username)) {
+        setUMes("userName is Valid");
+      } else if (!userRegEx.test(username) && username !== " ") {
+        setUMes("userName is Not Valid");
+      } else {
+        setUMes("");
+      }
+  
+    }
+    function submit(event){
+      event.preventDefault()
+      if(email === "" || username === "" || password=== ""){
+        alert("please enter something")
+      }else if(message === "Email is Not Valid" || pwmessage=== "pw is Not Valid" || uMes === "userName is Not Valid"){
+        alert("you have enter wrong details")
+      }
+      else{
+    const newUser={Email :email , Username : username , Password : password}
+    data.push(newUser)
+    setData([...data])
+    console.log(data)
+  
+    if(show=== true){
+      setShow(false)
+    }else{
+      setShow(true)
+    }
+      }
+      localStorage.setItem("list",JSON.stringify(data))
+  
+      setEmail("")
+      setMessage("")
+      setpassword("")
+      setPwMessage("")
+      setUMes("")
+      setusername("")
+  
+    }
+      return(
+          <>
+         <Navbar/>
+          <div className={RegisterStyle.wrap}>
+          <div className={RegisterStyle.box}>
+         {show ? <p style={{backgroundColour : "red" , color :"white"}}>HEY!  YOU HAVE SUCCESSFULLY SIGNED UP</p> : ""}
+          <div >
+          <label htmlFor="email"><MdMailOutline style={{fontSize:"2em"}}/></label>
+          <Input required name="email" className={RegisterStyle.input} placeholder="Email"
+           type="email" value={email} onChange={captureemail}/>
+          <p style={{color:"white" ,textShadow:"2px 2px red"}}>{message }</p>
+          </div>
+          <div>
+          <label><FaUserAlt style={{fontSize:"2em"}}/></label>
+          <Input value={username} onChange={captureuser} className={RegisterStyle.input}  placeholder="Username" type="text"/>
+          <p style={{color:"white" ,textShadow:"2px 2px red"}}>{uMes}</p>
+          </div>
+          <div>
+          <label><FaLock style={{fontSize:"2em"}}/></label>
+          <Input value={password} onChange={capturepass} className={RegisterStyle.input} 
+           placeholder="Password" type="password"/>
+          <p style={{color:"white" ,textShadow:"2px 2px red"}}>{pwmessage}</p>
+          <div>
+          <br/>
+          <div className={RegisterStyle.button}>
+          <Button className={RegisterStyle.submit} onClick={submit} text='Submit'/>
+          <NavLink className={RegisterStyle.Emoji} to ='/emoji'>😀Emoji</NavLink>
           </div>
           </div>
-      </div>
-   
-    </>
-  )
-}
+          </div>
+          
+          </div>
+          
+          </div>
+          </>
+      )
+  }
+ 
+         
+         
